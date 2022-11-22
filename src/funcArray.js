7c4a67c4042a334bed56d05c5bfa8f9be7b5b854
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2022-11-22 10:26:42 +0100

    solved in class

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,7 +1,20 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(){}
-export function findLongestWord(){}
+export function maxOfTwoNumbers(a,b){
+    return Math.max(a,b);    
+}
+export function findLongestWord(words){
+    let longestWord = "";
+    words.map(function(word){
+        if (word.length>longestWord.length){
+            longestWord = word;
+            }})
+        if (words.length==0){
+            return undefined
+        }
+        return longestWord;
+    }
+    
 export function sumArray(){}
 export function averageNumbers(){}
 export function averageWordLength(){}
